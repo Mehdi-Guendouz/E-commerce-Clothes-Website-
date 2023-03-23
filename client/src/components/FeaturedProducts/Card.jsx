a04c8753b85cfc0f5fdbff2e84f7a04ab8398722
Guendouz mehdi <mahdign12@gmail.com>
2023-03-23 15:48:11 +0100

    card component

--- client/src/components/FeaturedProducts/Card.jsx
+++ client/src/components/FeaturedProducts/Card.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./card.scss"
 
 const Card = () => {
     return (
-        <div>
-            card
-        </div>
+        <Link to={`/products/23`} className="link">
+            <div className='card'>
+                <div className="images">
+                    <span>New Season</span>
+                    <img src="./pic1.jpeg" alt="" className='mainImg'/>
+                    <img src="./pic2.jpeg" alt="" className='secondImg'/>
+                </div>
+                <h2>title</h2>
+                <div className="prices">
+                    <h3>40$</h3>
+                    <h3>19$</h3>
+                </div>
+            </div>
+        </Link>
     );
 }
 
---
+++ client/src/components/categories/Categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./categories.scss"
+
+const Categories = () => {
+    return (
+        <div>
+            category
+        </div>
+    );
+}
+
+export default Categories;
